ef0d3a97ec431e580ec61bed4526120970f37347
Preetinder325 <preetinder.singh@edu.sait.ca>
2024-01-31 02:24:56 -0700

    WebDev2
    Assignment3

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import ItemList from './item-list'; 
+import ItemList from './item-list';
 
 const Page = () => {
-    return (
-      <main className="p-4">
-        <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
-        <ul>
+  return (
+    <main className="bg-blue-100 p-5 min-h-screen">
+      <div className="max-w-2xl mx-auto">
+        <h1 className="text-2xl font-semibold text-center text-black mb-4">Shopping List</h1>
+        <ul className="bg-blue-300 rounded-lg p-5">
           {ItemList.map((item, index) => (
-            <li key={index}>
-              {item.name} - Quantity: {item.quantity} - Category: {item.category}
+            <li key={index} className="border-b py-2 ">
+              <div className="font-bold text-black">{item.name}</div>
+              <div className="text-black">Quantity : {item.quantity}</div>
+              <div className="text-black">Category : {item.category}</div>
             </li>
           ))}
         </ul>
-      </main>
-    );
-  };
-  
+      </div>
+    </main>
+  );
+};
 
 export default Page;

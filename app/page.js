3b79fc5a77e0a9980ca288308993cd21d86df83f
Preetinder325 <preetinder.singh@edu.sait.ca>
2024-03-18 08:45:04 -0600

    WebDev2
    Lab8

--- app/page.js
+++ app/page.js
@@ -6,22 +6,25 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-200 p-10">
       <h1 className="text-3xl font-bold text-black mb-6">CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2"className="bg-blue-400 mb-3 text-white font-bold py-2 px-4 rounded hover:bg-blue-800 transition duration-300">
-          Check my week 2 project !!!
+          Check my week 2 project
       </Link>
       <Link href="/week-3"className="bg-blue-400 mb-3 text-white font-bold py-2 px-4 rounded hover:bg-blue-800 transition duration-300 ">
-          Check my week 3 project !!!
+          Check my week 3 project 
       </Link>
       <Link href="/week-4"className="bg-blue-400 mb-3 text-white font-bold py-2 px-4 rounded hover:bg-blue-800 transition duration-300 ">
-          Check my week 4 project !!!
+          Check my week 4 project 
       </Link>
       <Link href="/week-5"className="bg-blue-400 mb-3 text-white font-bold py-2 px-4 rounded hover:bg-blue-800 transition duration-300  ">
-          Check my week 5 project !!!
+          Check my week 5 project 
       </Link>
       <Link href="/week-6"className="bg-blue-400 mb-3 text-white font-bold py-2 px-4 rounded hover:bg-blue-800 transition duration-300  ">
-          Check my week 6 project !!!
+          Check my week 6 project 
       </Link>
       <Link href="/week-7"className="bg-blue-400 mb-3 text-white font-bold py-2 px-4 rounded hover:bg-blue-800 transition duration-300  ">
-          Check my week 7 project !!!
+          Check my week 7 project 
+      </Link>
+      <Link href="/week-8"className="bg-blue-400 mb-3 text-white font-bold py-2 px-4 rounded hover:bg-blue-800 transition duration-300  ">
+          Check my week 8 project 
       </Link>
     </main>
   );
